030b9e8237e2132db06e3e37fc37c604f463915b
student 7 <unkown>
2024-03-17 16:54:37 +0300

    mount unmount ornek yapildi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import C1 from "./C1";
 
 function App() {
+  const [c1Kapali, c1AcKapa] = useState(false)
+  const [sayac, setSayac] = useState(0)
+
+  function c1Kapat() {
+    c1AcKapa( eskiDeger => !eskiDeger )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-5 bg-info-subtle p-5">
+          <button onClick={c1Kapat} className="btn btn-warning btn-sm my-3">C1 KAPAT</button>
+
+          {!c1Kapali && <C1 sayac={sayac} setSayac={setSayac} />}
+          
+      </div>
+    </>
+
   );
 }
 
