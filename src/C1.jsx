168a3d33cb340078ec5a3bbf0c2c4977930d442d
student 7 <unkown>
2024-03-17 18:07:04 +0300

    clean up function icin farkli ornekler yapildi

--- src/C1.jsx
+++ src/C1.jsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react"
 
-
 function C1() {
     const [sayac, setSayac] = useState(0);
+    const [baglanti, setBaglanti] = useState(navigator.onLine);
+    
     useEffect(() => {
-        const surekLiIslem1 = setInterval(() => console.log('started'), 2000);
+        function pencereDegisti(event) {
+            const yatayMi = (event.target.innerWidth / event.target.innerHeight) > 1 ? true : false;
+            console.log(yatayMi ? 'Yatay ekran' : "Dikey ekran");
+        }
 
+        window.addEventListener('resize',pencereDegisti);
         return () => {
-            clearInterval(surekLiIslem1);
-            console.log('Interval temizlendi....')
+            window.removeEventListener('resize', pencereDegisti);
         }
     }, []);
 
+    useEffect(() => {
+        const surekLiIslem = setInterval(() => {
+            setBaglanti(navigator.onLine);
+        }, 1000); 
+        return () => {
+            clearInterval(surekLiIslem);
+        }       
+    }, [])
     return (
         <>
             <div className="p-3 m-3 bg-success-subtle">
                 C1 Komponenti
                 <button className="btn btn-outline-danger" onClick={() => setSayac(eskiDeger => eskiDeger + 5)}>{sayac}</button>
             </div>
+            { !baglanti?
+            <div className="alert alert-danger p-3 position-fixed">
+                Baglanti Kesildi!
+            </div> : ""
+            } 
         </>
     )
 }
